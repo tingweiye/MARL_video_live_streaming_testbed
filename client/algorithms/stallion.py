c3690df839f689245aa626260f4815e9c9310081
tingweiye <ty2281@nyu.edu>
2023-10-22 23:57:26 -0400

    Fixed bugs

--- client/algorithms/stallion.py
+++ client/algorithms/stallion.py
@@ -19,7 +19,7 @@ class stallion_solver(object):
         self.bw_f = 1.0
         self.latency_f = 1.25
         self.n_step = 10
-        self.target_latency = 3.0
+        self.target_latency = 2.0
         self.speed_buffer_tth = 0.6
         self.bw_history = []
         self.latency_history = []
