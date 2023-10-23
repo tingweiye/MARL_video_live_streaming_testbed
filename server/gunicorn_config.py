79815cf41a2e59f56734028e2544993dd6238166
tingweiye <ty2281@nyu.edu>
2023-10-22 23:21:07 -0400

    Add Gunicorn configuartions. Keep-alive supported

---
+++ server/gunicorn_config.py
@@ -0,0 +1,12 @@
+# gunicorn_config.py
+
+# from videoServer.server import Server
+
+# server = Server()
+
+def on_starting(server):
+    # 这里可以添加其他启动时的操作
+    pass
+
+bind = "127.0.0.1:8080"
+workers = 1
