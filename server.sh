43601a54264ee51cc55daacc9c15384ea04b70c3
tingweiye <ty2281@nyu.edu>
2024-04-12 17:52:41 -0400

    Fixed bugs

--- server.sh
+++ server.sh
@@ -6,7 +6,7 @@ counter=0
 # echo $start_time
 
 # Execute test.py 10 times
-while [ $counter -lt 10 ]; do
+while [ $counter -lt 50 ]; do
     # Execute the Python script with counter as parameter
     echo $counter > .control
     gunicorn routing:application -c gunicorn_config.py
